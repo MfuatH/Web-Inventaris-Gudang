c1388cbfe1a7f72a742c0ecc0f2cb418c75f099e
Yusuf349113 <wigdeti@gmail.com>
2025-10-07 09:51:04 +0700

    push

--- resources/views/dashboard/partials/_admin-dashboard.blade.php
+++ resources/views/dashboard/partials/_admin-dashboard.blade.php
@@ -66,7 +66,7 @@
     <div class="p-6">
         <h3 class="flex items-center text-lg font-semibold mb-4 text-gray-800 dark:text-gray-200">
             <svg class="w-6 h-6 mr-2 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path></svg>
-            Daftar Stok item Yang Tipis
+            Daftar Stok Item Yang Tipis
         </h3>
         <div class="overflow-x-auto">
             <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
